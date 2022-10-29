3515a3940cf612760dd84d600fdccd0b4d93028f
Simisola <omotkleanbiz@gmail.com>
2022-10-29 08:34:13 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require ('fs');
+function onRequest(request, response) {
+    //const readStream = fs.createReadStream('.port-folio/index.html');
+    response.writeHead(200, {'Content-type': 'text/plain'});
+    fs.readFile('./index.html'), null, function(error, data){
+        if (error){
+            response.writeHead(404);
+            response.write('File not found');
+        } else{
+            response.write(data);
+        }
+        response.end();
+    }
+    
+};
